c6278e11151d53c80d5c60b629b435a8bded034e
Johannes Roth <johannes@roth24.de>
2018-07-01 15:00:27 +0200

    Change colors

--- hackathon-flink-scala/src/main/java/eu/streamline/hackathon/visualization/DataPlotter.java
+++ hackathon-flink-scala/src/main/java/eu/streamline/hackathon/visualization/DataPlotter.java
@@ -1,6 +1,10 @@
 package eu.streamline.hackathon.visualization;
 
+import com.typesafe.config.ConfigException;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
@@ -12,12 +16,14 @@ import javafx.stage.Stage;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 
 public class DataPlotter extends Application {
 
-    final String csvFilePath = "all-weather-mentions.csv";
-    final String stageTitle = "General tone by Temperature";
-    final String lineChartTitle = "Amount of Mentions by Precipitation";
+    final String csvFilePath = "all-events-weather-tone.csv";
+    final String stageTitle = "Riot tone by Temperature";
+    final String lineChartTitle = "General Tone by Temperature";
 
     @Override public void start(Stage stage) {
 
@@ -38,6 +44,7 @@ public class DataPlotter extends Application {
         final CategoryAxis xAxis = new CategoryAxis();
         final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Date");
+        yAxis.setLabel("Tone");
         //creating the chart
         final LineChart<String,Number> lineChart =
                 new LineChart<String,Number>(xAxis,yAxis);
@@ -47,12 +54,14 @@ public class DataPlotter extends Application {
         XYChart.Series toneLow = new XYChart.Series();
         XYChart.Series toneMid = new XYChart.Series();
         XYChart.Series toneHigh = new XYChart.Series();
-        toneLow.setName("toneLow");
-        toneMid.setName("toneMid");
-        toneHigh.setName("toneHigh");
+        toneLow.setName("Low temp");
+        toneMid.setName("Mid temp");
+        toneHigh.setName("High temp");
 
         //populating the series with data
+
         for (String[] row : csvData) {
+
             if(row[1].equals("low")) {
                 toneLow.getData().add(new XYChart.Data(row[0], Double.valueOf(row[2])));
             } else if(row[1].equals("mid")){
@@ -63,11 +72,49 @@ public class DataPlotter extends Application {
         }
 
         Scene scene  = new Scene(lineChart,800,600, Color.WHITE);
+
         lineChart.getData().add(toneLow);
         lineChart.getData().add(toneMid);
         lineChart.getData().add(toneHigh);
 
+        toneLow.getNode().setStyle("-fx-stroke: #6cccff");
+        toneMid.getNode().setStyle("-fx-stroke: #74e16d");
+        toneHigh.getNode().setStyle("-fx-stroke: #ff464c");
 
+        List<XYChart.Data<String, Number>> datapoints = toneLow.getData();
+        for(XYChart.Data<String, Number> data : datapoints){
+            data.getNode().setStyle("-fx-background-color: #6cccff, white");
+        }
+
+        datapoints = toneMid.getData();
+        for(XYChart.Data<String, Number> data : datapoints){
+            data.getNode().setStyle("-fx-background-color: #74e16d, white");
+        }
+
+        datapoints = toneHigh.getData();
+        for(XYChart.Data<String, Number> data : datapoints){
+            data.getNode().setStyle("-fx-background-color: #ff464c, white");
+        }
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                Set<Node> nodes = lineChart.lookupAll(".chart-legend-item-symbol.default-color0");
+                for (Node n : nodes) {
+                    n.setStyle("-fx-background-color: #6cccff, white");
+                }
+
+                nodes = lineChart.lookupAll(".chart-legend-item-symbol.default-color1");
+                for (Node n : nodes) {
+                    n.setStyle("-fx-background-color: #74e16d, white");
+                }
+
+                nodes = lineChart.lookupAll(".chart-legend-item-symbol.default-color2");
+                for (Node n : nodes) {
+                    n.setStyle("-fx-background-color: #ff464c, white");
+                }
+            }
+        });
 
         stage.setScene(scene);
         stage.show();
